28f93813216ae709777325b7d7d8dadba8f4f318
Josh Bowen <josh.bowen@stuxcd.io>
2021-11-14 16:28:44 +0000

    Add server command

--- cmd/server.go
+++ cmd/server.go
@@ -2,8 +2,17 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/stuxcd/custos-admission-controller/pkg/server"
+)
+
+var (
+	server_short = "run HTTP server"
+	server_long  = "run HTTP server on specified port"
 )
 
 func init() {
@@ -11,11 +20,29 @@ func init() {
 }
 
 var serverCmd = &cobra.Command{
-	Use:   "server",
-	Short: version_short,
-	Long:  version_long,
+	Use:   "server [port]",
+	Short: server_short,
+	Long:  server_long,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Printf("serving http\n")
+		if len(args) < 1 {
+			return fmt.Errorf("port is required")
+		}
+
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = args[0]
+		}
+
+		if _, err := strconv.Atoi(port); err != nil {
+			port = args[0]
+		}
+
+		stopCh := server.SetupSignalHandler()
+		jsonLogger, _ := NewLogger("info", "json")
+
+		keypath := cmd.Flag("keypath").Value.String()
+
+		server.ListenAndServe(port, time.Minute, jsonLogger, stopCh, keypath)
 		return nil
 	},
 }
